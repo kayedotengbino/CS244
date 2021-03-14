fd81c8c470a8af4fb9e35133b27fc561841ef7d9
Kaye Engbino <engbinop@gmail.com>
2021-03-15 02:05:18 +0800

    description

--- countSort.c
+++ countSort.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 void countSort(int arr[], int n, int place)
 {
     int i, freq[range] = {0};
@@ -20,3 +18,26 @@ void countSort(int arr[], int n, int place)
     for(i = 0; i < n; i++)
         arr[i] = output[i];
 }
+
+/*
+    Counting sort
+        an efficient algorithm for solving values with limited range
+        invented by Harold H. Seward in mid 1950s
+        Running time - O(n + k), n = size, k = size
+        can only sort integers
+        small integers with multiple counts
+
+    Harold H. Seward
+        invented theoritic sort and count sort in 1945
+        a computer scientist, inventor, engineer
+
+    Idea
+        1. Find the max element in an array
+        2. Initialize array length with max + 1
+        3. Store the count of elements in the index in array
+        4. Calculate commulative frequency of count array
+        5. Fix the values of the sorted array
+
+    Application
+         small ints with multiple counts
+*/
