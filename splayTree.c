35df2c522f282bb767bf31c4d8c85ece7f86f63f
Kaye Engbino <engbinop@gmail.com>
2021-03-17 08:39:09 +0800

    description

--- splayTree.c
+++ splayTree.c
@@ -10,10 +10,21 @@
             Insert
         most accessed element is near the root
             utilize the principle of locatily (80-20 rul) 
+        Running Time
+            O(Mlog n) - M - operations, M > n
+            O(log n) - average
+            Theta(n) - worst
 
     BST
         no balance condition
 
     Zig - right
     Zag - Left
+
+    Insert 
+        O(n^2) - not adjusted
+        O(1) - adjusted
+
+    Algorithm Design Technique
+        Divide and Conquer
 */
