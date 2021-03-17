176211aba8b521269f7fbfd75ae72f0d2fd846bf
Kaye Engbino <engbinop@gmail.com>
2021-03-17 08:31:32 +0800

    description

---
+++ splayTree.c
@@ -0,0 +1,19 @@
+/*
+    Splay Tree
+        invented by Daniel Dominic Seator and Robert Endre Tarjan in 1985
+        another kind of BST
+        self adjusting, self balancing 
+        Not balanced at all time
+        Performs basic operations - in amortized time
+            Search
+            Delete
+            Insert
+        most accessed element is near the root
+            utilize the principle of locatily (80-20 rul) 
+
+    BST
+        no balance condition
+
+    Zig - right
+    Zag - Left
+*/
